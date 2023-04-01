c3e3a37ad47b86f69bd2d5adfacb692053530bb4
Happy <nphuc22125@gmail.com>
2023-04-01 15:35:46 +0700

    ver2

--- controller/register.js
+++ controller/register.js
@@ -11,17 +11,42 @@ function getInfo(mala) {
   }
   return gender;
 }
-var registerArr=[]
+
+var check = new Validation();
 document.querySelector("#btn-register").onclick = function (e) {
     e.preventDefault();
-    var register = new UserRegister();
+    let register = new UserRegister();
     register.name = document.querySelector('#name').value;
     register.email = document.querySelector('#email').value;
-    register.password = document.querySelector('#password').value;register.phone = document.querySelector('#phone').value;
+    register.password = document.querySelector('#password').value;
+    register.passwordConFirm = document.querySelector('#password-confirm').value;
+
+    register.phone = document.querySelector('#phone').value;
     register.gender = getInfo(mala);
     
+    let valid = true;
+    valid = check.checkEmty(register.name,'e1')
+    &check.checkEmty(register.email,'e2')
+    &check.checkEmty(register.password,'e3')
+    &check.checkEmty(register.passwordConFirm,'e4')
+    &check.checkEmty(register.phone,'e5')
+
+    if (register.passwordConFirm != register.password) {
+      document.querySelector("#error-password").innerHTML = `Password ConFirm incorrect!`;
+      return;
+    } else {
+      document.querySelector("#error-password").innerHTML = " ";
+    
+    valid = check.checkLetter(register.name,'error-letter')
+    valid = check.checkemail(register.email,'error-email')
+    valid = check.checkPassword(register.password,'error-password')
+    valid = check.checkNumber(register.phone,'error-phone')
+    if(!valid){
+      return ;
+    }
     getRegister(register);
     console.log(register);
+}
 }
 
 
